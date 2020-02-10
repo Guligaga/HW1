01e4a3e192e0f9df0b986b7c1100b37fdb1b472b
Evgeniy Koval <glggboko@gmail.com>
2020-02-10 23:30:36 +0200

    after corrections
    Have divided task1 to task1...task5, have deleted task2...task5 from other folders.

---
+++ src/ifElse/task2.js
@@ -0,0 +1,24 @@
+"use strict"
+let x = -13.25, y = 7.9;
+if (x >= 0)
+{
+    if  (y >= 0)
+    {
+        alert ("I quarter");
+    }
+    else
+    {
+        alert ("IV quarter");
+    }
+}
+else
+{
+    if (y >= 0)
+    {
+        alert ("II quarter");
+    }
+    else
+    {
+        alert ("III quarter");
+    }
+}
---
+++ src/ifElse/task5.js
@@ -0,0 +1,16 @@
+"use strict"
+let rat = 79;
+if (rat >= 0 && rat <= 19)
+    alert("The mark is F");
+else if (rat >= 20 && rat <= 39)
+    alert("The mark is E");
+else if (rat >= 40 && rat <= 59)
+    alert("The mark is D");
+else if (rat >= 60 && rat <= 74)
+    alert("The mark is C");
+else if (rat >= 75 && rat <= 89)
+    alert("The mark is B");
+else if (rat >= 90 && rat <= 100)
+    alert("The mark is A");
+else
+    alert ("Error!");
